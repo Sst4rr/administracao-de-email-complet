91e061dc7431367854364c4b59e3362f7b0e621e
Thaís Ester Medeiros <thais.gomes@soulasalle.com.br>
2023-10-30 20:43:26 -0300

    primeiro edit, tem algum erro que ainda não achei, mas se eu mexer no que eu acho estar errado, ele arruma mais cinco erros ^^

---
+++ email.java
@@ -0,0 +1,29 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+ 
+    //constructor to receive the first name and last name
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.print("EMAIL CREATED: "+ this.firstName + " " + this.lastName);
+    }
+
+
+    //ask for the department
+
+    //generate a random password
+
+    //set the mailbox capacity
+
+    //set the alternate email
+
+    //change the password
+
+}
