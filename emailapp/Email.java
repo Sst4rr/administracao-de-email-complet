d5966be68671ec35bb1bc3ba4d8072e56d6d2ab3
Thaís Ester Medeiros <thais.gomes@soulasalle.com.br>
2023-10-31 02:23:42 -0300

    Temos a escolha de departamento pronta.

--- emailapp/Email.java
+++ emailapp/Email.java
@@ -1,4 +1,6 @@
 package emailapp;
+import java.util.Scanner;
+
 
 public class Email {
     private String firstName;
@@ -16,21 +18,22 @@ public class Email {
 
         //call a method asking for the departament - return the department
 
-
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
    
     }
 
 
     //ask for the department
-    // private String setDepartament(){
-    //     System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\0 for none");
-    //     Scanner in = new Scanner(System.in);
-    //     depChoice = in.nextInt();
-    //     if (depChoice == 1){ return "sales" ;}
-    //     else if (depChoice == 2){return "dev" ;}
-    //     else if (depChoice == 3){return "acct" ;}
-    //     else {return "";}
-    // }
+    private String setDepartment(){
+        System.out.print("DEPARTMENT CODE\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:\n");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1){ return "sales" ;}
+        else if (depChoice == 2){return "dev" ;}
+        else if (depChoice == 3){return "acct" ;}
+        else {return "";}
+    }
     
 
     //generate a random password
