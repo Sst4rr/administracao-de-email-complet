5e80ae4572da22a5a26896c8e9ae115b3f216c97
Thaís Ester Medeiros <thais.gomes@soulasalle.com.br>
2023-10-31 02:11:33 -0300

    TA RODANDO

--- emailapp/Email.java
+++ emailapp/Email.java
@@ -13,10 +13,25 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: "+ this.firstName + " " + this.lastName);
+
+        //call a method asking for the departament - return the department
+
+
+   
     }
 
 
     //ask for the department
+    // private String setDepartament(){
+    //     System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\0 for none");
+    //     Scanner in = new Scanner(System.in);
+    //     depChoice = in.nextInt();
+    //     if (depChoice == 1){ return "sales" ;}
+    //     else if (depChoice == 2){return "dev" ;}
+    //     else if (depChoice == 3){return "acct" ;}
+    //     else {return "";}
+    // }
+    
 
     //generate a random password
 
